08ae91aecd08b56f536026f7b68481ad36de2835
Leszek Czechak <leszekczechak@gmail.com>
2020-08-19 19:02:26 +0200

    WebClient used for RatesApi

--- src/main/java/com/czechak/leszek/your_budget/external/api/ratesapi/RatesApiClient.java
+++ src/main/java/com/czechak/leszek/your_budget/external/api/ratesapi/RatesApiClient.java
@@ -11,38 +11,30 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.time.LocalDate;
 
 @Component
 public class RatesApiClient {
 
-    private final String BASE_API = "https://api.ratesapi.io/api";
-    private final RestTemplate restTemplate;
+    private final WebClient webClient;
 
-    @Autowired
-    public RatesApiClient(RestTemplateBuilder restTemplateBuilder) {
-        this.restTemplate = restTemplateBuilder.build();
+    public RatesApiClient(WebClient webClient) {
+        this.webClient = webClient;
     }
 
     public CurrencyExchangeResponse currencyExchange(Currency sourceCurrency
             , Currency targetCurrency
             , LocalDate date) {
 
-        String apiUrl = BASE_API + "/" + date + "?base=" + targetCurrency + "&symbols=" + sourceCurrency;
+        String apiUrl = "/" + date + "?base=" + targetCurrency + "&symbols=" + sourceCurrency;
 
-        HttpHeaders httpHeaders= new HttpHeaders();
+        return webClient.get()
+                .uri(apiUrl)
+                .retrieve()
+                .bodyToMono(CurrencyExchangeResponse.class)
+                .block();
 
-        ResponseEntity<CurrencyExchangeResponse> response = restTemplate.exchange(
-                apiUrl,
-                HttpMethod.GET,
-                new HttpEntity<>(httpHeaders),
-                new ParameterizedTypeReference<CurrencyExchangeResponse>(){}
-        );
-
-        System.out.println(response.toString());
-
-        return response.getBody();
     }
-
 }
