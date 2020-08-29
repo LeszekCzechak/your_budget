c5c4fedef82d3f71e186ee7c54ec042bb5087924
Leszek Czechak <leszekczechak@gmail.com>
2020-08-29 18:30:57 +0200

    Testing part

--- src/test/java/com/czechak/leszek/your_budget/service/ExchangeRatesServiceTest.java
+++ src/test/java/com/czechak/leszek/your_budget/service/ExchangeRatesServiceTest.java
@@ -7,8 +7,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.reactivestreams.Publisher;
-
 import java.util.Collection;
 import java.util.LinkedList;
 
