5f0276298d48b91b2c40dacfbf738cc50d376314
Leszek Czechak <leszekczechak@gmail.com>
2020-08-29 15:07:38 +0200

    Testing part

--- src/test/java/com/czechak/leszek/your_budget/service/CategoryServiceTest.java
+++ src/test/java/com/czechak/leszek/your_budget/service/CategoryServiceTest.java
@@ -1,13 +1,23 @@
 package com.czechak.leszek.your_budget.service;
 
+import com.czechak.leszek.your_budget.dto.category.CreateCategoryRequest;
+import com.czechak.leszek.your_budget.model.CategoryEntity;
+import com.czechak.leszek.your_budget.model.UserEntity;
 import com.czechak.leszek.your_budget.repository.category.CategoryRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class CategoryServiceTest {
@@ -15,18 +25,37 @@ class CategoryServiceTest {
     @Mock
     private CategoryRepository categoryRepository;
     @Mock
-    private UserContext userContext;
+    UserContext userContext;
 
-    private CategoryService categoryService;
+    CategoryService categoryService;
 
     @BeforeEach
     void setUp() {
-        categoryService = new CategoryService(categoryRepository,userContext);
+        categoryService = new CategoryService(categoryRepository, userContext);
+
     }
 
     @Test
     void ShouldCreateCategory() {
 
+        //given
+        CreateCategoryRequest newCategory = new CreateCategoryRequest();
+        newCategory.setName("newCategory");
+        newCategory.setDescription("desc for test");
+
+        when(userContext.getCurrentUser().getUserId()).thenReturn(1L);
+
+        //when
+        categoryService.createCategory(newCategory);
+
+        //then
+        ArgumentCaptor<CategoryEntity> argumentCaptor = ArgumentCaptor.forClass(CategoryEntity.class);
+        Mockito.verify(categoryRepository).save(argumentCaptor.capture());
+        CategoryEntity categoryEntity = argumentCaptor.capture();
+
+        assertEquals("newCategory", categoryEntity.getName());
+        assertEquals("desc for test", categoryEntity.getDescription());
+        assertEquals(1L, categoryEntity.getUserId());
     }
 
     @Test
