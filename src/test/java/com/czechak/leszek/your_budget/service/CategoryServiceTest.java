de725d99da6d93e2030bdcae271ed712e52bfcd8
Leszek Czechak <leszekczechak@gmail.com>
2020-08-29 17:06:18 +0200

    Testing part

--- src/test/java/com/czechak/leszek/your_budget/service/CategoryServiceTest.java
+++ src/test/java/com/czechak/leszek/your_budget/service/CategoryServiceTest.java
@@ -1,9 +1,12 @@
 package com.czechak.leszek.your_budget.service;
 
+import com.czechak.leszek.your_budget.dto.category.CategoryResponse;
 import com.czechak.leszek.your_budget.dto.category.CreateCategoryRequest;
+import com.czechak.leszek.your_budget.dto.category.GetCategoryResponse;
 import com.czechak.leszek.your_budget.model.CategoryEntity;
 import com.czechak.leszek.your_budget.model.UserEntity;
 import com.czechak.leszek.your_budget.repository.category.CategoryRepository;
+import com.czechak.leszek.your_budget.repository.user.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -15,6 +18,9 @@ import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -24,6 +30,7 @@ class CategoryServiceTest {
 
     @Mock
     private CategoryRepository categoryRepository;
+
     @Mock
     UserContext userContext;
 
@@ -43,7 +50,9 @@ class CategoryServiceTest {
         newCategory.setName("newCategory");
         newCategory.setDescription("desc for test");
 
-        when(userContext.getCurrentUser().getUserId()).thenReturn(1L);
+        UserEntity userEntity = new UserEntity();
+        userEntity.setUserId(1L);
+        when(userContext.getCurrentUser()).thenReturn(userEntity);
 
         //when
         categoryService.createCategory(newCategory);
@@ -51,7 +60,7 @@ class CategoryServiceTest {
         //then
         ArgumentCaptor<CategoryEntity> argumentCaptor = ArgumentCaptor.forClass(CategoryEntity.class);
         Mockito.verify(categoryRepository).save(argumentCaptor.capture());
-        CategoryEntity categoryEntity = argumentCaptor.capture();
+        CategoryEntity categoryEntity = argumentCaptor.getValue();
 
         assertEquals("newCategory", categoryEntity.getName());
         assertEquals("desc for test", categoryEntity.getDescription());
@@ -59,14 +68,61 @@ class CategoryServiceTest {
     }
 
     @Test
-    void getCategories() {
+    void ShouldReturnAllCategories() {
+
+        //given
+        List<CategoryEntity> categoryEntities = new LinkedList<>();
+        CategoryEntity categoryEntity = new CategoryEntity(1L, 1L, "CategoryName", "CategoryDesc");
+        categoryEntities.add(categoryEntity);
+
+        UserEntity userEntity = new UserEntity();
+        userEntity.setUserId(1L);
+        when(userContext.getCurrentUser()).thenReturn(userEntity);
+        when(categoryRepository.findCategoryByUser(1L)).thenReturn(categoryEntities);
+
+        //when
+        GetCategoryResponse categories = categoryService.getCategories();
+
+        //then
+        assertEquals(1, categories.getCategories().size());
+        assertEquals(1L, categories.getCategories().get(0).getCategoryId());
+        assertEquals("CategoryName", categories.getCategories().get(0).getName());
+        assertEquals("CategoryDesc", categories.getCategories().get(0).getDescription());
+
     }
 
     @Test
-    void getCategory() {
+    void ShouldReturnCategoryByCategoryId() {
+        //given
+        CategoryEntity categoryEntity = new CategoryEntity(1L,3L,"CategoryName","CategoryDesc");
+
+        when(categoryRepository.findCategoryByCategoryId(1L)).thenReturn(categoryEntity);
+
+        //when
+        CategoryEntity category = categoryService.getCategory(1L);
+
+        //then
+        assertEquals(1L, category.getCategoryId());
+        assertEquals(3L, category.getUserId());
+        assertEquals("CategoryName", category.getName());
+        assertEquals("CategoryDesc", category.getDescription());
+
     }
 
     @Test
     void getCategoryResponseById() {
+        //given
+        CategoryEntity categoryEntity = new CategoryEntity(1L,3L,"CategoryName","CategoryDesc");
+
+        when(categoryRepository.findCategoryByCategoryId(1L)).thenReturn(categoryEntity);
+
+        //when
+        CategoryResponse category = categoryService.getCategoryResponseById(1L);
+
+        //then
+        assertEquals(1L, category.getCategoryId());
+        assertEquals("CategoryName", category.getName());
+        assertEquals("CategoryDesc", category.getDescription());
+
     }
 }
