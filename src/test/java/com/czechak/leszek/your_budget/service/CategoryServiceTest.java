25c357e82519eb77dc5663c865e901b32b9ca1ad
Leszek Czechak <leszekczechak@gmail.com>
2020-09-08 10:17:03 +0200

    Testing part

--- src/test/java/com/czechak/leszek/your_budget/service/CategoryServiceTest.java
+++ src/test/java/com/czechak/leszek/your_budget/service/CategoryServiceTest.java
@@ -6,23 +6,18 @@ import com.czechak.leszek.your_budget.dto.category.GetCategoryResponse;
 import com.czechak.leszek.your_budget.model.CategoryEntity;
 import com.czechak.leszek.your_budget.model.UserEntity;
 import com.czechak.leszek.your_budget.repository.category.CategoryRepository;
-import com.czechak.leszek.your_budget.repository.user.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.junit.MockitoJUnitRunner;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.LinkedList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
