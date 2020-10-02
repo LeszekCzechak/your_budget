48e67b02edbac635f8115d2c837f6f839c03401a
Leszek Czechak <leszekczechak@gmail.com>
2020-10-02 22:39:12 +0200

    Works Now

--- src/main/java/com/czechak/leszek/yourbudget/service/UserContext.java
+++ src/main/java/com/czechak/leszek/yourbudget/service/UserContext.java
@@ -1,10 +1,8 @@
 package com.czechak.leszek.yourbudget.service;
 
-import com.czechak.leszek.yourbudget.repository.user.UserRepository;
 import com.czechak.leszek.yourbudget.model.UserEntity;
 import org.springframework.stereotype.Component;
 
-import java.util.Optional;
 
 
 @Component
